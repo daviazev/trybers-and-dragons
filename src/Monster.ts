d8554d7bd0e720f4d72313dd927505433a417ffc
daviazevdo <azvd.davi@gmail.com>
2023-01-18 21:54:18 -0300

    requisito 9

---
+++ src/Monster.ts
@@ -0,0 +1,23 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 83) {}
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints - attackPoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+  
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
